7ce3a85089049803955d0796f85177771821fd2c
Tina Ye <tinaye@berkeley.edu>
2018-02-15 16:04:49 -0800

    yay!

--- foobar.rb
+++ foobar.rb
@@ -4,5 +4,11 @@ class Foobar
     # Class method, not an instance method
     # Call with `Foobar.baz`
     # Q4 CODE HERE
+    a.map! { |a| a.to_i }
+    a.map! { |a| a+2 }
+    a.reject! { |a| a % 2 != 0 }
+    a = a.uniq
+    a.reject! { |a| a >= 10 }
+    return a.inject(:+)
   end
 end
--- hw1.rb
+++ hw1.rb
@@ -1,16 +1,16 @@
 def squared_sum(a, b)
   # Q1 CODE HERE
-
+  return (a+b) ** 2
 end
 
 def sort_array_plus_one(a)
   # Q2 CODE HERE
-
+  return a.sort.map! { |a| a+1 }
 end
 
 def combine_name(first_name, last_name)
   # Q3 CODE HERE
-
+  return "#{first_name} #{last_name}"
 end
 
 def blockin_time(a)
@@ -49,4 +49,5 @@ def scrabble(word)
     z: 10,
   }
   # Q5 CODE HERE
+  return word.split('').map! { |item| values[item.to_sym] }.inject(0, :+)
 end
